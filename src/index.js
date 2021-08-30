042bb4eacc04f12dd62f6fae49b9a09283d7223f
Leytek <leytek@yandex.ru>
2021-08-30 11:05:29 +0300

    feat: implement check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = check = (str, bracketsConfig, stack = []) =>
+ str.split('').reduce((a, e) =>
+   a && bracketsConfig.reduce((aa, ee) =>
+    ((e === ee[0] && e !== ee[1]) ? (stack.push(e), aa) :
+     (e !== ee[0] && e === ee[1]) ? (aa && (stack.pop() === ee[0])) :
+      (e === ee[0] && e === ee[1]) ? stack[stack.length-1] === e ? (aa &&  (stack.pop() === e)) :
+       (stack.push(e), aa) : aa), true), true) && stack.pop() === undefined
