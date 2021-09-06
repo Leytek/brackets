9c9c86879e908319836236f35cc69d8a5e08119e
Leytek <leytek@yandex.ru>
2021-09-06 16:16:07 +0300

    refactor: refactor conditions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 module.exports = check = (str, bracketsConfig, stack = []) =>
- str.split('').reduce((a, e) =>
-   a && bracketsConfig.reduce((aa, ee) =>
-    ((e === ee[0] && e !== ee[1]) ? (stack.push(e), aa) :
-     (e !== ee[0] && e === ee[1]) ? (aa && (stack.pop() === ee[0])) :
-      (e === ee[0] && e === ee[1]) ? stack[stack.length-1] === e ? (aa &&  (stack.pop() === e)) :
-       (stack.push(e), aa) : aa), true), true) && stack.pop() === undefined
+  str.split('').reduce((a, e) =>
+    bracketsConfig.reduce((aa, ee) =>
+      a ? e === ee[0] ? e === ee[1] ? stack[stack.length-1] === e ? stack.pop() === e : (stack.push(e), aa) : (stack.push(e), aa) :
+      e === ee[1] ? stack.pop() === ee[0] : aa : a, true), true) && stack.pop() === undefined
